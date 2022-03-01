5ec5bb99165d0e44f2b00e1491305ee2f8d35c17
takano630 <t.takano@4nonome.com>
2022-03-01 13:39:40 +0900

    signup後自動login追加

--- user/views.py
+++ user/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.edit import CreateView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login, authenticate
 
 from .forms import AccountCreateForm
 
@@ -15,7 +16,11 @@ class SignupView(CreateView):
     
     def form_valid(self, form):
       user = form.save() 
-      return render(self.request, 'user/signup_successed.html') 
+      username = form.cleaned_data['username']
+      password = form.cleaned_data['password1']
+      user = authenticate(username=username, password=password)
+      login(self.request, user)
+      return redirect('home')
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
